a07e0978e9b7954b71fd1816bf955b59028d0818
Cyborgforge <sivasarvesh13@gmail.com>
2025-02-15 19:42:42 +0530

    Revert "Update App.jsx"
    This reverts commit ee8387ab5766bec5a1473a3eba4b47e2952f817f.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,15 +23,7 @@ function App() {
   const { isLoggedIn, isInitialized } = useAuth();
 
   if (!isInitialized) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <img
-          src="\src\assets\headerlogo.png" 
-          alt="Loading..."
-         className="w-10 h-5 animate-spin" // Tailwind animation for smooth effect
-        />
-      </div>
-    );
+    return <p>Loading...</p>;
   }
 
   return (
